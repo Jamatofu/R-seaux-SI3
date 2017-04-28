41e7fca2a6752bb9703acfcd3885e9de22cf768b
JulienMaureille <julien.maureille@live.fr>
2017-04-28 10:07:01 +0200

    query

--- src/main/java/JC/serveur/communication/Query.java
+++ src/main/java/JC/serveur/communication/Query.java
@@ -1,5 +1,7 @@
 package JC.serveur.communication;
 
+import java.util.List;
+
 /**
  * Created on 28/04/2017.
  * Polytech'Nice Sophia - SI3
@@ -7,4 +9,11 @@ package JC.serveur.communication;
  * @author Julien Maureille
  */
 public class Query {
+    private String resource; //sous forme : "type:id" -> par ex : "student:45"
+    /**
+     *
+     */
+    private String action; //ajouter, supprimer, lister...
+    private List<String> parameters;
+
 }
