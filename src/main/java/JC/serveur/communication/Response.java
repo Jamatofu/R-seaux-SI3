80a13426a248ce5904dad6e7e1db0e86c39ceb05
JulienMaureille <julien.maureille@live.fr>
2017-04-28 09:43:43 +0200

    Data

---
+++ src/main/java/JC/serveur/communication/Response.java
@@ -0,0 +1,10 @@
+package JC.serveur.communication;
+
+/**
+ * Created on 28/04/2017.
+ * Polytech'Nice Sophia - SI3
+ *
+ * @author Julien Maureille
+ */
+public class Response {
+}
---
+++ src/main/java/JC/serveur/data/Student.java
@@ -0,0 +1,10 @@
+package JC.serveur.data;
+
+/**
+ * Created on 28/04/2017.
+ * Polytech'Nice Sophia - SI3
+ *
+ * @author Julien Maureille
+ */
+public class Student {
+}
